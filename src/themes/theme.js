e4e7c9b2b484be6f9f448143bc1e53b3f4689b56
Chutarjs <chus2487tico@gmail.com>
2023-06-22 19:00:43 -0600

    cambios

---
+++ src/themes/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles' 
+export const appTheme = createTheme({
+     palette: { 
+        mode: 'light',
+        primary: { 
+            main: '#404bb0', 
+        }, 
+        secondary: { 
+            main: '#1eb0c3', 
+        }, 
+    },
+})
