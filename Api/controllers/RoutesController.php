c808cbf459cd3f98fd3d86ce33a46f71eb9ef7f6
Chutarjs <chus2487tico@gmail.com>
2023-06-22 19:01:39 -0600

    se añade la database

---
+++ Api/controllers/RoutesController.php
@@ -0,0 +1,11 @@
+<?php
+class RoutesController{
+    public function index(){
+        include "routes/routes.php";
+        /* 
+        includa once
+        require
+        require_once
+        */
+    }
+}
---
+++ Api/models/PlanModel.php
@@ -0,0 +1,75 @@
+<?php
+class PlanModel
+{
+    public $enlace;
+    public function __construct()
+    {
+        $this->enlace = new MySqlConnect();
+    }
+    /*Listar */
+    public function all(){
+        try {
+            //Consulta sql
+			$vSql = "SELECT * FROM ;";
+			
+            //Ejecutar la consulta
+			$vResultado = $this->enlace->ExecuteSQL ($vSql);
+				
+			// Retornar el objeto
+			return $vResultado;
+		} catch ( Exception $e ) {
+			die ( $e->getMessage () );
+		}
+    }
+    /*Obtener */
+    public function get($id)
+    {
+        try {
+            //Consulta sql
+			$vSql = "SELECT * FROM actor where id=$id";
+			
+            //Ejecutar la consulta
+			$vResultado = $this->enlace->ExecuteSQL ( $vSql);
+			// Retornar el objeto
+			return $vResultado;
+		} catch ( Exception $e ) {
+			die ( $e->getMessage () );
+		}
+    }
+    /*Obtener los actores de una pelicula */
+    public function getActorMovie($idMovie)
+    {
+        try {
+            //Consulta SQL
+            $vSQL = "SELECT g.id, g.fname, g.lname".
+            " FROM actor g, movie_cast mg".
+            " where g.id=mg.actor_id and mg.movie_id=$idMovie;";
+            //Establecer conexión
+            
+            //Ejecutar la consulta
+            $vResultado = $this->enlace->executeSQL($vSQL);
+            //Retornar el resultado
+            return $vResultado;
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+    }
+    public function getActorMovieForm($idMovie)
+    {
+        try {
+            //Consulta SQL
+            $vSQL = "SELECT mg.actor_id,mg.role".
+            " FROM actor g, movie_cast mg".
+            " where g.id=mg.actor_id and mg.movie_id=$idMovie;";
+            //Establecer conexión
+            
+            //Ejecutar la consulta
+            $vResultado = $this->enlace->executeSQL($vSQL);
+            //Retornar el resultado
+            return $vResultado;
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+    }
+    
+}
