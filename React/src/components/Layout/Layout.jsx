b7f659662b251196a04a7941547c018dc59a20cb
montes04 <montesalfarok@gmail.com>
2023-06-27 17:50:36 -0600

    se creo el react

---
+++ React/src/components/Layout/Layout.jsx
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-unused-vars
+import { React} from 'react'
+import  Header  from './Header';
+import { Footer } from './Footer';
+import { Container } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import {appTheme} from '../../themes/theme'
+
+// eslint-disable-next-line react/prop-types
+export function Layout({children}){ 
+    return(
+        <ThemeProvider theme={appTheme}>
+         <CssBaseline enableColorScheme />
+            <Header/>
+            <Container maxWidth='xl' style={{ paddingTop:'1rem', paddingBottom:'4.5rem'}}>
+            {children}
+            </Container>
+            <Footer />
+        </ThemeProvider>
+    );
+}
